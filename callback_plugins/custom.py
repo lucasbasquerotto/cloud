c5f955372f5380baf3b1e0637687e20569f07677
lucasbasquerotto <lucasbasquerotto@gmail.com>
2019-06-25 15:15:16 -0300

    fix message

--- callback_plugins/custom.py
+++ callback_plugins/custom.py
@@ -190,7 +190,7 @@ class CallbackModule(CallbackBase):
             if result._task.loop and 'results' in result._result:
                 self._process_items(result)
             else:
-                msg = "skipping456: [%s]" % result._host.get_name()
+                msg = "skipping: [%s]" % result._host.get_name()
                 if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and '_ansible_verbose_override' not in result._result:
                     msg += " => %s" % self._dump_results(result._result)
                 self._display.display(msg, color=C.COLOR_SKIP)
