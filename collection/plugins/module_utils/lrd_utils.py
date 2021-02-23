cd712bf594f60531b1bad4b1a7691c04a17a1e06
lucasbasquerotto <lucasbasquerotto@gmail.com>
2021-02-23 19:35:43 +0000

    [dev] node dns

--- collection/plugins/module_utils/lrd_utils.py
+++ collection/plugins/module_utils/lrd_utils.py
@@ -186,6 +186,15 @@ def load_cached_file(file_path):
   return file_result
 
 
+def ordered(obj):
+  if isinstance(obj, dict):
+    return sorted((k, ordered(v)) for k, v in obj.items())
+  if isinstance(obj, list):
+    return sorted(ordered(x) for x in obj)
+  else:
+    return obj
+
+
 def to_bool(value, default_value=None):
   if value is None:
     return default_value
