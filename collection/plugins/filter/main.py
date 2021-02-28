5731e5b965bc2ebe9d31032f03bc611aa1bb3c9b
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-02-28 10:56:46 -0300

    [dev] node dns

--- collection/plugins/filter/main.py
+++ collection/plugins/filter/main.py
@@ -53,17 +53,16 @@ class FilterModule(object):
 
     return result
 
-  def node_dns_service_info(self, dns_service, dns_service_params_list, active_host, state):
-    def prepare_item(item, dns_type, value):
+  def node_dns_service_info(self, dns_service, active_host, state):
+    ipv4 = active_host.get('public_ipv4') or ''
+    ipv6 = active_host.get('public_ipv6') or ''
+    ip_map = dict(A=ipv4, AAAA=ipv6)
+
+    def prepare_item(item):
+      value = ip_map.get(item.get('dns_type'))
+
       if (state != 'absent') and not value:
         return None
-
-      if item['dns_type']:
-        raise AnsibleError(
-            'the dns type is defined explicitly for the node dns service '
-            + 'record in the parameters list (should be let empty to be '
-            + 'defined later)'
-        )
 
       if item['value']:
         raise AnsibleError(
@@ -72,7 +71,6 @@ class FilterModule(object):
             + 'to be be defined later based on the node ip)'
         )
 
-      item['dns_type'] = dns_type
       item['value'] = value
       return item
 
@@ -93,9 +91,6 @@ class FilterModule(object):
     if (state == 'absent') and (not dns_service.get('can_destroy')):
       return skip_info
 
-    if not dns_service_params_list:
-      return skip_info
-
     params = dns_service.get('params') or dict()
 
     if params['dns_type']:
@@ -111,14 +106,9 @@ class FilterModule(object):
           + 'based on the node ip)'
       )
 
-    ipv4 = active_host.get('public_ipv4') or ''
-    ipv6 = active_host.get('public_ipv6') or ''
-    ip_map = dict(A=ipv4, AAAA=ipv6)
-
     params_list_aux = [
-        prepare_item(item, dns_type=t, value=ip_map.get(t))
+        prepare_item(item)
         for item in (dns_service_params_list or [])
-        for t in ['A', 'AAAA']
     ]
     params_list = [i for i in params_list_aux if i is not None]
 
